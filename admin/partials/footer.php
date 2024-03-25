dd338c8b6b6609cfd719df5f44ef52dbc1449998
Magno Reis <maagnoreiz@gmail.com>
2024-03-25 03:32:18 -0300

    footer

---
+++ admin/partials/footer.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Erick Collins - Sistema Jurídico</span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Copyright © 2024. All rights reserved.</span>
+          </div>
+        </footer>
