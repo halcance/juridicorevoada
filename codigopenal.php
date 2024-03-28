12ed42bb888ccc9f81cc3df58c2ae296c2a0b58e
UrsoFg <andreyfgurgel@gmail.com>
2024-03-28 05:12:59 -0300

    cod. processsual finalizado

---
+++ codigopenal.php
@@ -0,0 +1,289 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<?php 
+$page_title = "Regimento Interno";
+include('includes/head.php');
+
+?>
+
+<body>
+  <!--::header part start::-->
+  <?php include('includes/header.php') ?>
+  <!-- Header part end-->
+
+
+  <!-- breadcrumb start-->
+  <section class="breadcrumb breadcrumb_bg">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="breadcrumb_iner text-center">
+            <div class="breadcrumb_iner_item">
+            <h2>Código Penal</h2>
+              <p><span>O Código Penal é um conjunto de leis que estabelece os crimes e suas respectivas penas. Ele determina as condutas consideradas ilícitas e define as consequências jurídicas para aqueles que as praticam. O Código Penal visa proteger a sociedade, promover a justiça e prevenir a prática de crimes.</span></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- breadcrumb start-->
+
+  <!-- ================ contact section start ================= -->
+  <section class="contact-section section_padding">
+    <div class="container">
+      <div class="d-none d-sm-block mb-5 pb-4">
+  <p></p>
+      </div>
+
+
+      <div class="row">
+        <div class="col-12">
+          <h2 class="contact-title">Capítulo I - DISPOSIÇÕES GERAIS</h2>
+          <br>
+          <br>
+          <p>⚖️ <strong>Art. 1</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 2</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 3</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 4</strong> </p>
+          <br>
+        </div>
+        <div class="col-12">
+          <h2 class="contact-title">Capítulo II - CRIMES CONTRA A VIDA</h2>
+          <br>
+          <br>
+          <br>
+          <p>⚖️ <strong>Art. 5</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 6</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 7</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 8</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 9</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 10</strong> </p>
+          <br>
+        </div>
+        <div class="col-12">
+          <h2 class="contact-title">Capítulo III - CRIMES CONTRA DIREITOS FUNDAMENTAIS</h2>
+          <br>
+          <br>
+          <p>⚖️ <strong>Art. 11</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 12</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 13</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 14</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 15</strong> </p>
+          <br>
+        </div>
+        <div class="col-12">
+          <h2 class="contact-title">Capítulo IV - CRIMES CONTRA A LIBERDADE PESSOAL</h2>
+          <br>
+          <br>
+          <p>⚖️ <strong>Art. 16</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 17</strong> </p>
+          <br>
+        </div>
+        <div class="col-12">
+          <h2 class="contact-title">Capítulo V - CRIMES CONTRA A LIBERDADE PESSOAL</h2>  
+          <br>
+          <br>
+          <p>⚖️ <strong>Art. 18</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 19</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 20</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 21</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 22</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 23</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 24</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 25</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 26</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 27</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 28</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 29</strong> </p>
+          <br>
+        </div>
+        <div class="col-12">
+          <h2 class="contact-title">Capítulo VI - CRIMES CONTRA A ORDEM PÚBLICA</h2>
+          <br>
+          <br>
+          <p>⚖️ <strong>Art. 30</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 31</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 32</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 33</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 34</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 35</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 36</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 37</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 38</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 39</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 30</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 31</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 32</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 33</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 34</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 35</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 36</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 37</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 38</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 39</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 40</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 41</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 42</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 43</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 44</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 45</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 46</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 47</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 48</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 49</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 50</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 51</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 52</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 53</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 54</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 55</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 56</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 57</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 58</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 59</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 60</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 61</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 62</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 63</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 64</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 65</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 66</strong> </p>
+          <br>
+        </div>
+        <div class="col-12">
+          <h2 class="contact-title">Capítulo VII - CRIMES DE TRÂNSITO</h2>
+          <br>
+          <br>
+          <p>⚖️ <strong>Art. 67</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 68</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 69</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 70</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 71</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 72</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 73</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 74</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 75</strong> </p>
+          <br>
+          <p>⚖️ <strong>Art. 76</strong> </p>
+          <br>
+
+        </div>
+      
+    </div>
+  </section>
+  <!-- ================ contact section end ================= -->
+
+  <!-- footer part start-->
+  <?php include('includes/footer.php') ?>
+  <!-- footer part end-->
+
+  <!-- jquery plugins here-->
+  <!-- jquery -->
+  <script src="js/jquery-1.12.1.min.js"></script>
+  <!-- popper js -->
+  <script src="js/popper.min.js"></script>
+  <!-- bootstrap js -->
+  <script src="js/bootstrap.min.js"></script>
+  <!-- easing js -->
+  <script src="js/jquery.magnific-popup.js"></script>
+  <!-- swiper js -->
+  <script src="js/swiper.min.js"></script>
+  <!-- swiper js -->
+  <script src="js/masonry.pkgd.js"></script>
+  <!-- particles js -->
+  <script src="js/owl.carousel.min.js"></script>
+  <!-- swiper js -->
+  <script src="js/slick.min.js"></script>
+  <script src="js/gijgo.min.js"></script>
+  <script src="js/jquery.nice-select.min.js"></script>
+  <!-- contact js -->
+  <script src="js/jquery.ajaxchimp.min.js"></script>
+  <script src="js/jquery.form.js"></script>
+  <script src="js/jquery.validate.min.js"></script>
+  <script src="js/mail-script.js"></script>
+  <script src="js/contact.js"></script>
+  <!-- custom js -->
+  <script src="js/custom.js"></script>
+</body>
+
+</html>
