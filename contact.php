60802d80e014d63d90987dadc81bd819453552cd
UrsoFg <andreyfgurgel@gmail.com>
2024-03-28 03:03:58 -0300

    finalizado

--- contact.php
+++ contact.php
@@ -43,8 +43,209 @@ include('includes/head.php');
           <h2 class="contact-title">Capítulo I - DISPOSIÇÕES GERAIS</h2>
           <br>
           <br>
-          <p>⚖️ Art. 1 Não é permitido ingressar no Poder Judiciário estando com ficha criminal na polícia.</p>
+          <p>⚖️ <strong>Art. 1</strong> Não é permitido ingressar no Poder Judiciário estando com ficha criminal na polícia.</p>
+          <br>
+          <p>⚖️ <strong>Art. 2</strong> É vedado o desvio de recursos material ou imaterial pertencentes ao poder judiciário. O desvio se aplica também a utilização de bens em sua posse para uso pessoal.</p>
+          <br>
+          <p>⚖️ <strong>Art. 3</strong> Todas as alterações e aplicações devem ser postadas no diário respectivo para fins de conhecimento público. Quaisquer alterações só são válidas após publicação no diário oficial.</p>
+          <br>
+          <p>⚖️ <strong>Art. 4</strong> É proibido o vazamento de informações sigilosas, especialmente processos que ocorrem em segredo de justiça.</p>
+          <br>
+          <p>⚖️ <strong>Art. 5</strong> É vedada a postulação de processos a desfavor da Prefeitura.</p>
+          <br>
+          <p>⚖️ <strong>Art. 6</strong> Deve-se obedecer a hierarquia interna. Todos aqueles que exercem função jurídica devem colaborar com os órgãos de aferição e controle na execução do seu trabalho.</p>
+          <br>
         </div>
+        <div class="col-12">
+          <h2 class="contact-title">Capítulo II - DA ADVOCACIA</h2>
+          <br>
+          <br>
+          <p>⚖️ <strong>Art. 7</strong> As atividades da advocacia são:</p>
+          <br>
+          <ul>
+                <li>I - a abertura de processos jurídicos;</li>
+                <li>II - as atividades de consultoria, assessoria e direção jurídicas;</li>
+                <li>III - as atividades de documentação e registros públicos.</li>
+            </ul>
+            <br>
+            <p>⚖️ <strong>Art. 8</strong> O advogado, em primeira instância, além de ser defensor do estado democrático, é indispensável para a administração da justiça, o mesmo ainda se faz presente preservador da paz social. Por tanto são deveres do mesmo:
+
+            </p>
+            <br>
+            <ul>
+              <li>I -  preservar, de modo profissional, sua conduta, nobreza, ordem e dignidade profissionais no momento em que zelar, essencialmente, pelo seu caráter e indispensabilidade; </li>
+              <li>II - procurar aperfeiçoamento e empenho em sua função;</li>
+              <li>III - contribuir de toda e qualquer forma, para o cumprimento das leis e dos direitos existentes em caso;</li>
+              <li>IV - encorajar uma conversa entre litigantes com a finalidade de resolver quaisquer diferenças e mitigar instauração de novos litígios, além de aconselhar o cliente a não entrar em uma aventura judicial</li>
+
+            </ul>
+            <br>
+            <p>⚖️ <strong>Art. 9</strong> O advogado não deverá aceitar como cliente alguém que já tenha iniciado tal processo com outro advogado, sem antes a informação e interação prévia por parte do cliente, salvo com ordens do juiz.</p>
+            <br>
+            <br>
+            <p>⚖️ <strong>Art. 10</strong> É direito e dever de um profissional de direito, como sendo advogado, aceitar a defesa criminal, desconsiderando sua opinião pessoal sobre o caso.</p>
+            <br>
+            <br>
+            <p>⚖️ <strong>Art. 11</strong> O sigilo profissional acontece, independentemente da profissão, deve ser respeitado, salvo quando o profissional advogado receber ameaças e atentados contra a sua vida, sua honra, ou quando o mesmo se ver afrontado pelo próprio cliente, que em defesa pessoal, poderá revelar segredos, considerando a restrição ao interesse da causa.</p>
+            <br>
+        </div>
+        <div class="col-12">
+          <h2 class="contact-title">Capítulo III - DO PODER JUDICIÁRIO</h2>
+          <br>
+          <br>
+          <p>⚖️ <strong>Art. 12</strong> O ingresso na carreira da magistratura inicia-se através de concurso público do tribunal de justiça.</p>
+          <br>
+            <p>⚖️ <strong>Art. 13</strong> São deveres do magistrado:</p>
+            <br>
+            <ul>
+              <li>I -  Cumprir suas funções com serenidade, exatidão e independência;</li>
+              <li>II - determinar as providências necessárias para que os atos processuais se realizem nos prazos legais;</li>
+              <li>III - exercer assídua fiscalização sobre os subordinados, especialmente no que se refere à cobrança de custas e emolumentos, embora não haja reclamação das partes.</li>
+
+            </ul>
+            <br>
+            <p>⚖️ <strong>Art. 14</strong> É vedado ao judiciário:</p>
+            <br>
+            <ul>
+              <li>I -  Dar declarações ou manifestações sobre processos pendentes;</li>
+              <li>II - Assumir casos em que seja tratado como réu ou alvo principal.</li>
+            </ul>
+            <br>
+            <p>⚖️ <strong>Art. 15</strong> O Magistrado irá responder por perdas ou danos quando:</p>
+            <br>
+            <ul>
+              <li>I -  prosseguir com fraude no exercício da função;</li>
+              <li>II - recusar, omitir ou retardar sem motivo providência que deva ordenar o ofício, ou o requerimento das partes.</li>
+            </ul>
+            <br>
+            <p>⚖️ <strong>Art. 16</strong> O exercício da magistratura exige conduta compatível com os preceitos deste Código e pelos princípios da independência, da imparcialidade, do conhecimento e capacitação, da cortesia, da transparência, do segredo profissional, da prudência, da diligência, da integridade profissional e pessoal, da dignidade, da honra e do decoro.</p>
+            <br>
+            <br>
+            <p>⚖️ <strong>Art. 17</strong> A atividade judicial deve desenvolver-se de modo a garantir e fomentar a dignidade da pessoa humana, objetivando assegurar e promover a solidariedade e a justiça na relação entre as pessoas.</p>
+            <br>
+            <br>
+            <p>⚖️ <strong>Art. 18</strong> Exige-se do magistrado que seja eticamente independente e que não interfira, de qualquer modo, na atuação jurisdicional de outro colega, exceto em respeito às normas legais.</p>
+            <br>
+            <br>
+            <p>⚖️ <strong>Art. 19</strong> Impõe-se ao magistrado pautar-se no desempenho de suas atividades sem receber indevidas influências externas e estranhas à justa convicção que deve formar para a solução dos casos que lhe sejam submetidos.</p>
+            <br>
+            <br>
+            <p>⚖️ <strong>Art. 20</strong>  O magistrado imparcial é aquele que busca nas provas a verdade dos fatos, com objetividade e fundamento, mantendo ao longo de todo o processo uma distância equivalente das partes, e evita todo o tipo de comportamento que possa refletir favoritismo, predisposição ou preconceito.</p>
+            <br>
+            <br>
+            <p>⚖️ <strong>Art. 21</strong> Não se considera tratamento discriminatório injustificado:</p>
+            <br>
+            <ul>
+              <li>I -  a audiência concedida a apenas uma das partes ou seu advogado, contanto que se assegure igual direito à parte contrária, caso seja solicitado;</li>
+              <li>II - o tratamento diferenciado resultante de lei.</li>
+            </ul>
+            <br>
+            <p>⚖️ <strong>Art. 22</strong> A atuação do magistrado deve ser transparente, documentando-se seus atos, sempre que possível, mesmo quando não legalmente previsto, de modo a favorecer sua publicidade, exceto nos casos de sigilo contemplado em lei.</p>
+            <br>
+            <br>
+            <p>⚖️ <strong>Art. 23</strong> O magistrado, obedecido o segredo de justiça, tem o dever de informar ou mandar informar aos interessados acerca dos processos sob sua responsabilidade, de forma útil, compreensível e clara.</p>
+            <br>
+            <br>
+            <p>⚖️ <strong>Art. 24</strong> É dever do magistrado recusar benefícios ou vantagens de ente público, de empresa privada ou de pessoa física que possam comprometer sua independência funcional.</p>
+            <br>
+            <br>
+            <p>⚖️ <strong>Art. 25</strong> O magistrado deve manter atitude aberta e paciente para receber argumentos ou críticas lançados de forma cortês e respeitosa, podendo confirmar ou retificar posições anteriormente assumidas nos processos em que atua.</p>
+            <br>
+
+        </div>
+        <div class="col-12">
+          <h2 class="contact-title">Capítulo IV - DOS PODERES</h2>
+          <br>
+          <br>
+            <p>⚖️ <strong>Art. 26</strong> O Tribunal de Justiça é constituído da seguinte forma hierárquica:</p>
+            <br>
+            <P>Desembargador</p>
+
+            <P>Juiz de Direiro</p>
+
+            <P>Diretor</p>
+
+            <P>Conselheiro</p>
+
+            <P>Advogado Master</p>
+
+            <P>Advogado Sênior</p>
+
+            <P>Advogado Pleno</p>
+
+            <P>Advogado Júnior</p>
+
+            <P>Defensor Público</p>
+
+            <P>Estagiário</p>
+
+            <br>
+            <p>⚖️ <strong>Art. 27</strong> Funções de cada cargo:</p>
+            <br>
+            
+            <p>Desembargador: despacho de sentenças em 2º instância, supervisão, observância das leis.</p>
+            <p>Juiz de Direito: expedição de documentos, despacho de sentenças em 1º instância, supervisão da advocacia.</p>
+            <p>Diretoria: gestão de pessoas, supervisão da comissão de exame de ordem.</p>
+            <p>Conselho: consulta, atendimento, expedição de documentos, supervisão de advogados, aplicação de concursos, aplicação de cursos, admissão.</p>
+            <p>Advogado: realização de consulta, atendimento, expedição de documentos.</p>
+            <p>Estagiário: auxiliar os demais membros do judiciário na consultoria através de acompanhamento, postagem de relatório entre outros serviços solicitados pelos mesmos.</p>
+            <br>
+            <p>⚖️ <strong>Art. 28</strong> Advogado Júnior não poderá assumir processos sozinhos, sendo obrigatório o acompanhamento por um Advogado Sênior, salvo casos com autorização do Conselho.</p>
+            <br>
+            <br>
+            <p>⚖️ <strong>Art. 29</strong> Só poderá ter organizações como cliente os que estiverem no cargo de Advogado Sênior ou acima.</p>
+            <br>
+            <br>
+            <p>⚖️ <strong>Art. 30</strong> Aqueles que trabalham no judiciário devem exercer de forma independente, não podendo possuir outro vínculo empregatício. Exceto Defensor Público, pois há autorização do conselho e direção</p>
+            <br>
+        </div>
+        <div class="col-12">
+          <h2 class="contact-title">Capítulo V - DA CONDUTA</h2>
+          <br>
+          <br>
+            <p>⚖️ <strong>Art.31</strong> Durante o exercício da função é obrigatório a utilização de roupas sociais e em audiências é obrigatório o uso de terno.</p>
+            <br>
+            <br>
+            <p>⚖️ <strong>Art. 32</strong> Aqueles que exercem cargos jurídicos representam o judiciário, devendo manter uma boa postura e aparência.</p>
+            <br>
+            <ul>
+              <li>I -  É vedado aos membros do poder judiciário a participação ou manifestação de processos ou condutas ilegais. Fica autorizado a participação de ações ilegais somente com a prévia autorização da Corregedoria.</li>
+              
+            </ul>
+            <br>
+            <p>⚖️ <strong>Art. 33</strong> Obrigatório o uso do crachá (MASCULINO: 64 / FEMININO: 62) durante representações judiciais.</p>
+            <br>
+        </div>
+        <div class="col-12">
+          <h2 class="contact-title">Capítulo VI - DAS PENALIDADES</h2>
+          <br>
+          <br>
+            <p>⚖️ <strong>Art. 34</strong> As penalidades são:</p>
+            <br>
+            <p>Advertência Verbal</p>
+            <p>Advertência¹</p>
+            <p>Advertência²</p>
+            <p>Exoneração</p>
+            <br>
+            <p>⚖️ <strong>Art. 35</strong> O acúmulo ou reincidência em advertências poderá ocasionar em exoneração imediata.</p>
+            <br>
+            <br>
+            <p>⚖️ <strong>Art. 36</strong> A ausência das atividades jurídicas injustificadas em prazos maiores que três (03) dias poderá acarretar em exoneração imediata.</p>
+            <br>
+        </div>
+        <div class="col-12">
+          <h2 class="contact-title">Capítulo VII - DEFENSORIA PÚBLICA</h2>
+          <br>
+          <br>
+            <p>⚖️ <strong>Art. 37</strong> A Defensoria Pública fica limitado a assistência jurídica.</p>
+            <br>
+            <br>
+            <p>⚖️ <strong>Art. 38</strong> Necessário pagamento de taxa semanal, na falta a licença será suspensa.</p>
+            <br>
+          
+        </div>
+        <p><i class="bi bi-asterisk"></i> Atualizado em 28 de Março de 2024</p>
+
       
       </div>
     </div>
