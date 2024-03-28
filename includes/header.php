d4531a3b8134ceb5f8a8a73f274ed47bc1d8d336
Magno Reis <maagnoreiz@gmail.com>
2024-03-28 02:44:51 -0300

    ok

--- includes/header.php
+++ includes/header.php
@@ -15,9 +15,7 @@
                                 <li class="nav-item">
                                     <a class="nav-link" href="index.php">Início</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="about.php">Sobre</a>
-                                </li>
+                                
                                 <li class="nav-item">
                                     <a class="nav-link" href="service.php">Serviços</a>
                                 </li>
